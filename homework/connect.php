79f01dc54b61dfa19b495285ff821df86e13e9ea
Lex Dex <41371569+lexdex@users.noreply.github.com>
2018-11-26 00:02:27 +0200

    Add files via upload

---
+++ homework/connect.php
@@ -0,0 +1,14 @@
+<?php
+$settings = [
+    'host' => 'mariadb',
+    'dbname' => 'drupal_lab',
+    'user' => 'drupal_lab',
+    'password' => 'drupal_lab',
+];
+try{
+    $pdo = new PDO("mysql:host=" .$settings['host'] . ";dbname=" .$settings['dbname'], $settings['user'], $settings['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: I can't connect " . $e->getMessage());
+}
+?>
