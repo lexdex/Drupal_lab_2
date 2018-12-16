f4fc2cd5c1aef5f2679a63c0dd89b80fb8b2f3d6
Lex Dex <41371569+lexdex@users.noreply.github.com>
2018-12-17 01:22:43 +0200

    Add files via upload

---
+++ !Readme.txt
@@ -0,0 +1,16 @@
+Fast Start
+Make a git clone of HM4 or download it
+Open dir HM4/homework/src/index.hhml
+Thats all
+-------------------------------------------
+Long Start
+
+Make a git clone of HM4 or download it
+Download node and install it
+Now you should to download npm
+Open in HM4 dir 
+Use command npm i and download all plugins using in this project
+
+You can build project using gulp gulpfile.js 
+Project will be build in dist folder
+Source files of the project are in the src folder
---
+++ homework/src/scripts/script.js
@@ -0,0 +1,34 @@
+$(function($) {
+    $('#searchbutton').click(() => {
+        $("#content").empty();
+        var searchTerm =($('#searchrequest').val());
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+          $.ajax({
+            type: "GET",
+            url: url,
+            async: false,
+            data: "data",
+            dataType: 'jsonp',
+            success: (data, status, jqXHR) => {
+            $('#content').html();
+            for(let i = 0;i < data[1].length; i++) {
+                    $('#content').prepend("<div><div class='content-items'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>"+"<br>");
+                }
+                if ($('#content').html() == '') {
+                    $('.errorBox').css({'display': 'block'});
+                }
+                else {
+                    $('.errorBox').css({'display': 'none'});
+                }
+            },
+            error : (errorMsg) => {
+                alert('Wiki API isnt  currently working');
+            }        
+        });
+    });
+    $('#searchrequest').keypress((e) => { 
+        if(e.which == 13) {
+            $('#searchbutton').click();
+        }
+    });
+});  
